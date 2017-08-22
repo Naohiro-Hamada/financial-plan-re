9460a73b0bb8d165c063c439d4aa19c0603a600b
NaohiroHamada <fj6363jg@aa.jp.fujitsu.com>
2017-08-22 16:03:46 +0900

    routing設定

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  { path: 'family', loadChildren: './family/family.module#FamilyModule' },
+  { path: 'income-confirmation', loadChildren: './income-confirmation/income-confirmation.module#IncomeConfirmationModule' },
+  { path: 'balance-sheet', loadChildren: './balance-sheet/balance-sheet.module#BalanceSheetModule' },
+  { path: 'life-event', loadChildren: './life-event/life-event.module#LifeEventModule' },
+  { path: 'cash-flow', loadChildren: './cash-flow/cash-flow.module#CashFlowModule' },
+  { path: '', redirectTo: 'family', pathMatch: 'full' }
 ];
 
 @NgModule({
@@ -13,3 +15,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
---
+++ src/app/cash-flow/cash-flow.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fp-cash-flow',
+  templateUrl: './cash-flow.component.html',
+  styleUrls: ['./cash-flow.component.css']
+})
+export class CashFlowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
