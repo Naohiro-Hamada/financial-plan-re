443df49f1510372c8289a5462eed111b64d4a5a8
NaohiroHamada <fj6363jg@aa.jp.fujitsu.com>
2017-08-23 11:49:12 +0900

    family-list追加

---
+++ src/app/family/family-list/family-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fp-family-list',
+  templateUrl: './family-list.component.html',
+  styleUrls: ['./family-list.component.css']
+})
+export class FamilyListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/family/family.module.ts
+++ src/app/family/family.module.ts
@@ -4,12 +4,13 @@ import { CommonModule } from '@angular/common';
 import { FamilyRoutingModule } from './family-routing.module';
 import { FamilyComponent } from './family.component';
 import { FamilyInputComponent } from './family-input/family-input.component';
+import { FamilyListComponent } from './family-list/family-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FamilyRoutingModule
   ],
-  declarations: [FamilyComponent, FamilyInputComponent]
+  declarations: [FamilyComponent, FamilyInputComponent, FamilyListComponent]
 })
 export class FamilyModule { }
